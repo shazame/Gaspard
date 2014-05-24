19fab01a86c6e022da0ac679d06ed8104631c58c
David Bitonneau <david.bitonneau@gmail.com>
2014-05-24 15:03:48 +0200

    Gaspard can be controlled with a smartphone using bluetooth.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <wirish/wirish.h>
 #include <servos.h>
 #include <terminal.h>
+#include <commands.h>
+#include <rc.h>
+#include <math.h>
 #include <main.h>
 
 #include "Motors.hpp"
@@ -13,16 +16,20 @@ TERMINAL_PARAMETER_DOUBLE(walkPeriod, "Walking period", 1.5);
 TERMINAL_PARAMETER_DOUBLE(walkAngle, "Walking angle", 50);
 
 
-#define STATE_INIT 0
-#define STATE_WALK 1
+#define STATE_INIT 1
+#define STATE_WALK 2
 #define STATE_RESET 10
-TERMINAL_PARAMETER_INT(state, "Robot current state", STATE_INIT);
+TERMINAL_PARAMETER_INT(state, "Robot current state", 0);
+TERMINAL_PARAMETER_INT(joypad_state, "Joypad state", 0);
+
+#define DEG2RAD(a) ((a) * M_PI / 180.0)
 
 /**
  * Writing initialization code here
  */
 void setup()
 {
+    terminal_to_usb();
     motors_init();
     motors_enable_all();
 }
@@ -38,10 +45,6 @@ void tick()
     switch(state) {
         case STATE_INIT:
             motors_init_position_all();
-            if (t > 3) {
-                t = 0;
-                state = STATE_WALK;
-            }
             break;
         case STATE_WALK:
             walk(t, walkPeriod, walkAngle);
@@ -55,9 +58,61 @@ void tick()
     }
 }
 
+volatile float a1, r1, a2, r2;
+volatile float jx1, jy1, jx2, jy2;
+
+void joypad_flush()
+{
+    /* Cartesian coordinates */
+    jx1 = cos(DEG2RAD(a1))*r1;
+    jy1 = sin(DEG2RAD(a1))*r1;
+    jx2 = cos(DEG2RAD(a2))*r2;
+    jy2 = sin(DEG2RAD(a2))*r2;
+}
+
+void joypad_push(unsigned char c)
+{
+    switch (joypad_state) {
+        case 0:
+            if (c == 0x55) {
+                ++joypad_state;
+            }
+            if (c == 'B') {
+                start_stop();
+            }
+            if (c == 'C') {
+                state = STATE_INIT;
+            }
+            if (c == 'D') {
+                state = STATE_WALK;
+            }
+            break;
+        case 1:
+            r1 = c;
+            ++joypad_state;
+            break;
+        case 2:
+            a1 = c*10.0;
+            ++joypad_state;
+            break;
+        case 3:
+            r2 = c;
+            ++joypad_state;
+            break;
+        case 4:
+            a2 = c*10.0;
+            joypad_flush();
+            joypad_state = 0;
+            break;
+    }
+}
+
 /**
  * Loop method, called everytime
  */
 void loop()
 {
+    while (RC.available()) {
+        joypad_push(RC.read());
+    }
 }
