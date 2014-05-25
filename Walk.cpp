434cfa3688d54c9349822d96869cfbf64324a65a
David Bitonneau <david.bitonneau@gmail.com>
2014-05-25 22:46:51 +0200

    Walk: commented debug io

--- Walk.cpp
+++ Walk.cpp
@@ -13,69 +13,69 @@ void walkReset(void)
     ratio = 0;
 }
 
-void walkMoveSmooth(FrontLeg &fl, RearLeg &bl, double from_a, double to_a, double ratio)
+static void walkMoveSmooth(FrontLeg &fl, RearLeg &rl, double from_a, double to_a, double ratio)
 {
-    terminal_io()->println(ratio);
+    //terminal_io()->println(ratio);
 
-    bl.moveGrounded(from_a + ratio * (to_a - from_a));
+    rl.moveGrounded(from_a + ratio * (to_a - from_a));
     fl.moveGrounded(from_a + ratio * (to_a - from_a));
 }
 
 void walk(double dt, double walkSpeed, double walkAngle)
 {
-    double step_ratio = 1. / 8;
+    double stepRatio = 1. / 8;
 
-    ratio += (step_ratio * dt * walkSpeed) / walkAngle;
+    ratio += (stepRatio * dt * walkSpeed) / walkAngle;
     if (ratio > 1)
         ratio -= 1;
     else if (ratio < 0)
         ratio += 1;
 
     // Step 1: 2 legs (1) up and forward
-    if (ratio < step_ratio) {
-        terminal_io()->print("Step 1:");
-        walkMoveSmooth(leftFrontLeg, rightRearLeg, 0, walkAngle, ratio / step_ratio);
+    if (ratio < stepRatio) {
+        //terminal_io()->print("Step 1:");
+        walkMoveSmooth(leftFrontLeg, rightRearLeg, 0, walkAngle, ratio / stepRatio);
     }
 
     // Step 2: move forward by moving backward the 2 other legs (2), 2 legs (1) get down
-    else if (ratio < 2*step_ratio) {
-        terminal_io()->print("Step 2:");
-        walkMoveSmooth(rightFrontLeg, leftRearLeg, 0, -walkAngle, (ratio - step_ratio) / step_ratio);
+    else if (ratio < 2*stepRatio) {
+        //terminal_io()->print("Step 2:");
+        walkMoveSmooth(rightFrontLeg, leftRearLeg, 0, -walkAngle, (ratio - stepRatio) / stepRatio);
     }
 
     // Step 3: 2 legs (1) back to initial position
-    else if (ratio < 3*step_ratio) {
-        terminal_io()->print("Step 3:");
-        walkMoveSmooth(leftFrontLeg, rightRearLeg, walkAngle, 0, (ratio - 2*step_ratio) / step_ratio);
+    else if (ratio < 3*stepRatio) {
+        //terminal_io()->print("Step 3:");
+        walkMoveSmooth(leftFrontLeg, rightRearLeg, walkAngle, 0, (ratio - 2*stepRatio) / stepRatio);
     }
 
     // Step 4: 2 legs (2) back to initial position
-    else if (ratio < 4*step_ratio) {
-        terminal_io()->print("Step 4:");
-        walkMoveSmooth(rightFrontLeg, leftRearLeg, -walkAngle, 0, (ratio - 3*step_ratio) / step_ratio);
+    else if (ratio < 4*stepRatio) {
+        //terminal_io()->print("Step 4:");
+        walkMoveSmooth(rightFrontLeg, leftRearLeg, -walkAngle, 0, (ratio - 3*stepRatio) / stepRatio);
     }
 
     // Step 5: 2 legs (2) up and forward
-    else if (ratio < 5*step_ratio) {
-        terminal_io()->print("Step 5:");
-        walkMoveSmooth(rightFrontLeg, leftRearLeg, 0, walkAngle, (ratio - 4*step_ratio) / step_ratio);
+    else if (ratio < 5*stepRatio) {
+        //terminal_io()->print("Step 5:");
+        walkMoveSmooth(rightFrontLeg, leftRearLeg, 0, walkAngle, (ratio - 4*stepRatio) / stepRatio);
     }
 
     // Step 6: 2 legs (1) backward, 2 legs (2) get down
-    else if (ratio < 6*step_ratio) {
-        terminal_io()->print("Step 6:");
-        walkMoveSmooth(leftFrontLeg, rightRearLeg, 0, -walkAngle, (ratio - 5*step_ratio) / step_ratio);
+    else if (ratio < 6*stepRatio) {
+        //terminal_io()->print("Step 6:");
+        walkMoveSmooth(leftFrontLeg, rightRearLeg, 0, -walkAngle, (ratio - 5*stepRatio) / stepRatio);
     }
 
     // Step 7: 2 legs (2) back to initial position
-    else if (ratio < 7*step_ratio) {
-        terminal_io()->print("Step 7:");
-        walkMoveSmooth(rightFrontLeg, leftRearLeg, walkAngle, 0, (ratio - 6*step_ratio) / step_ratio);
+    else if (ratio < 7*stepRatio) {
+        //terminal_io()->print("Step 7:");
+        walkMoveSmooth(rightFrontLeg, leftRearLeg, walkAngle, 0, (ratio - 6*stepRatio) / stepRatio);
     }
 
     // Step 8: 2 legs (1) back to initial position
     else {
-        terminal_io()->print("Step 8:");
-        walkMoveSmooth(leftFrontLeg, rightRearLeg, -walkAngle, 0, (ratio - 7*step_ratio) / step_ratio);
+        //terminal_io()->print("Step 8:");
+        walkMoveSmooth(leftFrontLeg, rightRearLeg, -walkAngle, 0, (ratio - 7*stepRatio) / stepRatio);
     }
 }
